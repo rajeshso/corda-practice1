6deee6bcdfd073ba38f7462f09020d56ed3d3d4b
rajeshso <fun_rajesh@yahoo.com>
2018-06-04 16:56:39 +0100

    Basics, Issuance to own vault

--- cordapp-contracts-states/src/main/kotlin/com/template/StatesAndContracts.kt
+++ cordapp-contracts-states/src/main/kotlin/com/template/StatesAndContracts.kt
@@ -1,33 +1,87 @@
 package com.template
 
-import net.corda.core.contracts.CommandData
-import net.corda.core.contracts.Contract
-import net.corda.core.contracts.ContractState
+import net.corda.core.contracts.*
 import net.corda.core.identity.AbstractParty
 import net.corda.core.transactions.LedgerTransaction
+import net.corda.core.schemas.QueryableState
+import net.corda.core.schemas.MappedSchema
+import net.corda.core.schemas.PersistentState
+import java.security.PublicKey
+import java.util.*
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.Table
+import net.corda.core.contracts.Requirements.using
 
 // *****************
 // * Contract Code *
 // *****************
 // This is used to identify our contract when building a transaction
-val TEMPLATE_CONTRACT_ID = "com.template.TemplateContract"
+val INVOICE_CONTRACT_ID = "com.template.InvoiceContract"
 
-class TemplateContract : Contract {
+class InvoiceContract : Contract {
+
     // A transaction is considered valid if the verify() function of the contract of each of the transaction's input
     // and output states does not throw an exception.
     override fun verify(tx: LedgerTransaction) {
-        // Verification logic goes here.
+        val command = tx.commands.requireSingleCommand<Commands>()
+        command.value.verify(tx, command.signers)
     }
 
     // Used to indicate the transaction's intent.
     interface Commands : CommandData {
-        class Action : Commands
+        fun verify(tx: LedgerTransaction, signers: List<PublicKey>)
+
+        class IssueAction : Commands {
+            override fun verify(tx: LedgerTransaction, signers: List<PublicKey>) {
+               "No input should be present" using (tx.inputStates.isEmpty())
+               "Atleast one input should be present" using (tx.outputStates.isNotEmpty())
+            }
+        }
     }
 }
 
 // *********
 // * State *
 // *********
-data class TemplateState(val data: String) : ContractState {
-    override val participants: List<AbstractParty> get() = listOf()
+data class InvoiceState(override val linearId: UniqueIdentifier,
+                        val supplier: AbstractParty,
+                        val funder: AbstractParty?,
+                        val invoiceDesc: String) : LinearState, QueryableState {
+    override fun generateMappedObject(schema: MappedSchema): PersistentState {
+        return when(schema) {
+            is InvoiceSchemaV1 -> InvoiceSchemaV1.PersistentInvoiceSchemaV1(
+                    linearId = this.linearId.id,
+                    funder = this.funder,
+                    supplier = this.supplier,
+                    description = this.invoiceDesc
+            )
+            else -> throw IllegalArgumentException("Only Invoice is supported at this moment")
+        }
+     }
+
+    override fun supportedSchemas(): Iterable<MappedSchema> =  listOf<MappedSchema>(InvoiceSchemaV1)
+
+    override val participants: List<AbstractParty> get() = listOfNotNull(supplier, funder)
+}
+
+
+object InvoiceSchemaV1 : MappedSchema(
+        schemaFamily = InvoiceSchema.javaClass,
+        version = 1,
+        mappedTypes = listOf(PersistentInvoiceSchemaV1::class.java)
+) {
+    @Entity
+    @Table(name = "invoices")
+    class PersistentInvoiceSchemaV1(
+            var linearId: UUID,
+            val funder: AbstractParty?,
+            val supplier: AbstractParty,
+            val description: String
+            ) : PersistentState()
+
+}
+
+object InvoiceSchema {
+
 }
--- cordapp/src/main/kotlin/com/template/App.kt
+++ cordapp/src/main/kotlin/com/template/App.kt
@@ -1,16 +1,26 @@
 package com.template
 
 import co.paralleluniverse.fibers.Suspendable
+import net.corda.core.contracts.UniqueIdentifier
 import net.corda.core.flows.*
+import net.corda.core.identity.CordaX500Name
 import net.corda.core.messaging.CordaRPCOps
+import net.corda.core.messaging.vaultQueryBy
+import net.corda.core.serialization.CordaSerializable
 import net.corda.core.serialization.SerializationWhitelist
+import net.corda.core.transactions.SignedTransaction
+import net.corda.core.utilities.ProgressTracker
 import net.corda.webserver.services.WebServerPluginRegistry
+import java.util.*
 import java.util.function.Function
-import javax.ws.rs.GET
-import javax.ws.rs.Path
-import javax.ws.rs.Produces
 import javax.ws.rs.core.MediaType
 import javax.ws.rs.core.Response
+import net.corda.core.contracts.Command
+import net.corda.core.transactions.TransactionBuilder
+import com.template.INVOICE_CONTRACT_ID
+import net.corda.core.messaging.startTrackedFlow
+import net.corda.core.utilities.getOrThrow
+import javax.ws.rs.*
 
 // *****************
 // * API Endpoints *
@@ -22,7 +32,32 @@ class TemplateApi(val rpcOps: CordaRPCOps) {
     @Path("templateGetEndpoint")
     @Produces(MediaType.APPLICATION_JSON)
     fun templateGetEndpoint(): Response {
+        return Response
+                .status(Response.Status.OK)
+                .entity("Template GET endpoint")
+                .build()
         return Response.ok("Template GET endpoint.").build()
+    }
+
+    @POST
+    @Path("issue")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    fun issueInvoice(message:  InvoiceMessage): Response {
+        try {
+            val flowHandle = rpcOps.startTrackedFlow(::Initiator, message)
+            flowHandle.progress.subscribe { println(">> $it") }
+            val result = flowHandle.returnValue.getOrThrow()
+            return Response
+                    .status(Response.Status.CREATED)
+                    .entity("Invoice created")
+                    .build()
+        } catch (ex: Throwable) {
+            return Response
+                    .status(Response.Status.BAD_REQUEST)
+                    .entity(ex.message)
+                    .build()
+        }
     }
 }
 
@@ -31,12 +66,67 @@ class TemplateApi(val rpcOps: CordaRPCOps) {
 // *********
 @InitiatingFlow
 @StartableByRPC
-class Initiator : FlowLogic<Unit>() {
-    @Suspendable
-    override fun call() {
-        return Unit
+class Initiator(private val invoice: InvoiceMessage) : FlowLogic<SignedTransaction>() {
+    companion object {
+        object GENERATING_TRANSACTION : ProgressTracker.Step("Generating transaction based on new invoice.")
+        object VERIFYING_TRANSACTION : ProgressTracker.Step("Verifying contracts constraints.")
+        object SIGNING_TRANSACTION : ProgressTracker.Step("Signing transaction with our private key.")
+        object GATHERING_SIGNATURES : ProgressTracker.Step("Gathering the counterparty's signature.") {
+            override fun childProgressTracker() = CollectSignaturesFlow.tracker()
+        }
+
+        object FINALISING_TRANSACTION : ProgressTracker.Step("Obtaining notary signature and recording transaction.") {
+            override fun childProgressTracker() = FinalityFlow.tracker()
+        }
+        fun tracker() = ProgressTracker(
+                GENERATING_TRANSACTION,
+                VERIFYING_TRANSACTION,
+                SIGNING_TRANSACTION,
+                GATHERING_SIGNATURES,
+                FINALISING_TRANSACTION
+        )
     }
+    override val progressTracker = tracker()
+
+    @Suspendable
+    override fun call() : SignedTransaction {
+        val notary = serviceHub
+                .networkMapCache
+                .notaryIdentities.first()
+        val supplier =  serviceHub.myInfo.legalIdentities.first()
+
+        //Step 1 - Create Unsigned Transaction
+        progressTracker.currentStep = GENERATING_TRANSACTION
+        val outputState = InvoiceState(UniqueIdentifier(), supplier,null, invoice.description )
+
+        val command = Command(
+                value=InvoiceContract.Commands.IssueAction(),
+                signers=outputState.participants.map { it.owningKey })
+
+        val transactionBuilder = TransactionBuilder(notary)
+                .addOutputState(outputState, INVOICE_CONTRACT_ID)
+                .addCommand(command)
+
+        //Step 2 - Verify Transaction
+        progressTracker.currentStep = VERIFYING_TRANSACTION
+        transactionBuilder.verify(serviceHub)
+
+        //Step 3 - Sign the Transaction
+        progressTracker.currentStep = SIGNING_TRANSACTION
+        val selfSignedTransaction = serviceHub.signInitialTransaction(transactionBuilder)
+
+
+        // Stage 4 - Finalize transaction
+        progressTracker.currentStep = FINALISING_TRANSACTION
+        return subFlow(FinalityFlow(
+                transaction = selfSignedTransaction,
+                progressTracker = FINALISING_TRANSACTION.childProgressTracker()))
+    }
+
 }
+
+@CordaSerializable
+data class InvoiceMessage (val description: String)
 
 @InitiatedBy(Initiator::class)
 class Responder(val counterpartySession: FlowSession) : FlowLogic<Unit>() {
